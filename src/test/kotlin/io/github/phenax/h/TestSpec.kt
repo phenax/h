13042df58807c81601d8eca2a2ab148cb2bec39d
Akshay Nair <phenax5@gmail.com>
2017-11-11 16:15:14 +0530

    Enabled stdout logging for tests

--- src/test/kotlin/io/github/phenax/h/TestSpec.kt
+++ src/test/kotlin/io/github/phenax/h/TestSpec.kt
@@ -1,13 +1,40 @@
-import io.kotlintest.matchers.shouldBe
+package io.github.phenax.h
+
+import io.kotlintest.matchers.*
+import io.kotlintest.properties.*
 import io.kotlintest.specs.ShouldSpec
+import io.kotlintest.*
+
 
 class TestSpec : ShouldSpec() {
+
+	/**
+	 * Test case interceptor (Before test and after test)
+	 */
+	private val testCaseInterceptor: (TestCaseContext, () -> Unit) -> Unit = { _, test ->
+		test()
+	}
+
+	// Test config
+	override val defaultTestCaseConfig: TestCaseConfig =
+		TestCaseConfig(
+			interceptors = listOf(testCaseInterceptor)
+		)
+
+	override val oneInstancePerTest = true
+
 	init {
 		"String.length" {
 			should("return the length of the string") {
 				"sammy".length shouldBe 5
 				"".length shouldBe 0
 			}
+
+			/*should("eat my dick") {
+				forAll({ a: String ->
+					true
+				})
+			}*/
 		}
 	}
 }
