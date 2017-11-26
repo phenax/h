ef3a1e6fce3c49b8e666301cf1b3655c9d274a05
Akshay Nair <phenax5@gmail.com>
2017-11-26 19:31:07 +0530

    Added inline component example

--- examples/kotlin/src/main/kotlin/MainClass.kt
+++ examples/kotlin/src/main/kotlin/MainClass.kt
@@ -1,10 +1,17 @@
 package kotlinexample
 
+import io.github.phenax.h.*
 
 fun main(args: Array<String>) {
 
-	val component = MyComponent()
-	val html = component.renderToHtml()
+	val comp = MyComponent()
+	val html = comp.renderToHtml()
 
 	println(html)
+	println(component {
+		div(null, listOf(
+			h1(null, "Hello"),
+			text("yo ma boey")
+		))
+	}.renderToHtml())
 }
